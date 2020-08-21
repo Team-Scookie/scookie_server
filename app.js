cd13db70cadf469caf4906282d0ba12a8ecd98b6
hyex <hyexkim@gmail.com>
2020-08-21 22:13:04 +0900

    Init: Express Init

---
+++ app.js
@@ -0,0 +1,62 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+const port = process.env.PORT || 4500;
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Body-parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Node.js의 native Promise 사용
+mongoose.Promise = global.Promise;
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+// error handler
+// app.use((err, req, res, next) => {
+app.use((err, req, res) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+// CONNECT TO MONGODB SERVER
+mongoose.connect(process.env.MONGO_URI, { useMongoClient: true })
+  .then(() => console.log('Successfully connected to mongodb'))
+  .catch((e) => console.error(e));
+
+app.listen(port, () => console.log(`Server listening on port ${port}`));
+
+module.exports = app;
