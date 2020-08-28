0fefe3fc5e9a5d7a5b994397d5091515b0c00fa9
YeongWoo <rdd9223@gmail.com>
2020-08-28 21:15:43 +0900

    fix: port죽는 문제 해결

--- app.js
+++ app.js
@@ -1,29 +1,28 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-require('dotenv').config();
+// require("dotenv").config();
 
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
 
 const app = express();
-const port = process.env.PORT || 4500;
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Body-parser
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -32,8 +31,8 @@ app.use(bodyParser.json());
 // Node.js의 native Promise 사용
 mongoose.Promise = global.Promise;
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
@@ -45,18 +44,22 @@ app.use((req, res, next) => {
 app.use((err, req, res) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 // CONNECT TO MONGODB SERVER
-mongoose.connect(process.env.MONGO_URI, { useMongoClient: true })
-  .then(() => console.log('Successfully connected to mongodb'))
+mongoose
+  .connect(
+    "mongodb+srv://admin:admin@scookie1.bn06y.mongodb.net/scookie?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Successfully connected to mongodb"))
   .catch((e) => console.error(e));
 
-app.listen(port, () => console.log(`Server listening on port ${port}`));
+// app.listen(port, () => console.log(`Server listening on port ${port}`));
 
 module.exports = app;
