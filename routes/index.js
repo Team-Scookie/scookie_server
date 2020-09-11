5b480545fe5d69eea04a28969fd20e6f740b3238
hyex <hyexkim@gmail.com>
2020-09-12 00:08:40 +0900

    test: Add test router for insert point document

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,18 @@
 const express = require("express")
+const Point = require('../models/point')
 
 const router = express.Router()
 
 /* GET home page. */
 router.get("/", (req, res) => {
   res.render("index", { title: "Express" })
+})
+
+// Create new point document
+router.post("/test/point/", (req, res) => {
+  Point.create(req.body)
+    .then((todo) => res.send(todo))
+    .catch((err) => res.status(500).send(err))
 })
 
 module.exports = router
