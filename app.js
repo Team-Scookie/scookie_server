78ab594167e3df1e30ecde62591f6434875d2b39
hyex <hyexkim@gmail.com>
2020-09-11 22:14:31 +0900

    feat: apply dotenv and lint

--- app.js
+++ app.js
@@ -1,61 +1,64 @@
-const createError = require("http-errors");
-const express = require("express");
-const path = require("path");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
+const createError = require("http-errors")
+const express = require("express")
+const path = require("path")
+const cookieParser = require("cookie-parser")
+const logger = require("morgan")
 
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
+const mongoose = require("mongoose")
+const bodyParser = require("body-parser")
+const dotenv = require("dotenv")
 
-const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
+const indexRouter = require("./routes/index")
+const usersRouter = require("./routes/users")
 
-const app = express();
+const app = express()
+
+dotenv.config()
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"))
+app.set("view engine", "ejs")
 
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(logger("dev"))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, "public")))
 
 // Body-parser
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 // Node.js의 native Promise 사용
-mongoose.Promise = global.Promise;
+mongoose.Promise = global.Promise
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use("/", indexRouter)
+app.use("/users", usersRouter)
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-  next(createError(404));
-});
+  next(createError(404))
+})
 
 // error handler
 // app.use((err, req, res, next) => {
 app.use((err, req, res) => {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.message = err.message
+  res.locals.error = req.app.get("env") === "development" ? err : {}
 
   // render the error page
-  res.status(err.status || 500);
-  res.render("error");
-});
+  res.status(err.status || 500)
+  res.render("error")
+})
 
 // CONNECT TO MONGODB SERVER
 mongoose
   .connect(
-    "mongodb+srv://admin:admin@scookie1.bn06y.mongodb.net/scookie?retryWrites=true&w=majority",
-    { useNewUrlParser: true, useUnifiedTopology: true }
+    process.env.MONGO_URI,
+    { useNewUrlParser: true, useUnifiedTopology: true },
   )
   .then(() => console.log("Successfully connected to mongodb"))
-  .catch((e) => console.error(e));
+  .catch((e) => console.error(e))
 
-module.exports = app;
+module.exports = app
