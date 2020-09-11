dc794d26f805184ac1a029ebb29684eb247c055a
hyex <hyexkim@gmail.com>
2020-09-12 00:17:53 +0900

    fix: add user_id in point collection

--- models/point.js
+++ models/point.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose")
 const pointSchema = new mongoose.Schema(
   {
     point_id: { type: Number, required: true, unique: true },
+    user_id: { type: Number, required: true },
     latitude: { type: String, required: true },
     longitude: { type: String, required: true },
   },
