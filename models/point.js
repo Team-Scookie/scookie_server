ef59c89ed4699860fb3bc27ef9d62f677a42cbf4
hyex <hyexkim@gmail.com>
2020-09-26 18:11:33 +0900

    Refactor: Change DB schema

--- models/point.js
+++ models/point.js
@@ -1,15 +1,10 @@
 const mongoose = require("mongoose")
 
-const pointSchema = new mongoose.Schema(
-  {
-    point_id: { type: Number, required: true, unique: true },
-    user_id: { type: Number, required: true },
-    latitude: { type: String, required: true },
-    longitude: { type: String, required: true },
-  },
-  {
-    timestamps: true,
-  },
-)
+const pointSchema = new mongoose.Schema({
+  userId: { type: String, required: true },
+  latitude: { type: String, required: true },
+  longitude: { type: String, required: true },
+  timestamp: { type: Date, required: true },
+})
 
 module.exports = mongoose.model("Point", pointSchema)
