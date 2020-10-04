ace50104cc54ff34c6a614aef96a165aca5a2aa4
hyex <hyexkim@gmail.com>
2020-10-04 17:10:48 +0900

    Refactor: change point schema
    - add marker column
    - marker column is object and has 3 values.
    - add timestamp

--- models/point.js
+++ models/point.js
@@ -1,10 +1,19 @@
 const mongoose = require("mongoose")
 
-const pointSchema = new mongoose.Schema({
-  userId: { type: String, required: true },
-  latitude: { type: String, required: true },
-  longitude: { type: String, required: true },
-  timestamp: { type: Date, required: true },
-})
+const pointSchema = new mongoose.Schema(
+  {
+    userId: { type: String, required: true },
+    latitude: { type: String, required: true },
+    longitude: { type: String, required: true },
+    marker: {
+      address: { type: String },
+      placeName: { type: String },
+      elapsedTime: { type: Number }, // seconds
+    },
+  },
+  {
+    timestamps: true,
+  },
+)
 
 module.exports = mongoose.model("Point", pointSchema)
