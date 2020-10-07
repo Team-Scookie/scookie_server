b388922eb02ef1b29b3b424a9eb88817956fa48d
rdd9223 <rdd9223@gmail.com>
2020-10-08 00:31:32 +0900

    Feat: add user.signin

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,5 @@
 const express = require("express")
 const bcrypt = require("bcrypt")
-const Todo = require("../models/todo")
 const User = require("../models/user")
 const { authUtil, responseMessage, statusCode, jwt } = require("../tools")
 
@@ -10,7 +9,7 @@ const router = express.Router()
 
 // Find All
 router.get("/", (req, res) => {
-  Todo.findAll()
+  User.findAll()
     .then(users => {
       if (!users.length) return res.status(404).send({ err: "users not found" })
       return res.send(`find successfully: ${users}`)
@@ -55,6 +54,36 @@ router.post("/signup", async (req, res) => {
   return res
     .status(statusCode.CREATED)
     .send(authUtil.successTrue(responseMessage.X_CREATE_SUCCESS("USER"), { signinUser, token }))
+})
+
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body
+
+  if (!email || !password) {
+    return res.status(statusCode.BAD_REQUEST).send(authUtil.successFalse(responseMessage.NULL_VALUE))
+  }
+
+  const getUserInfoResult = await User.findOne({ email })
+  if (!getUserInfoResult) {
+    return res.status(statusCode.BAD_REQUEST).send(authUtil.successFalse(responseMessage.NO_X("USER")))
+  }
+
+  const isCorrectPassword =
+    getUserInfoResult.password &&
+    bcrypt.compareSync(password, getUserInfoResult.password, (err, response) => {
+      console.log(err, response)
+      return res
+        .status(statusCode.INTERNAL_SERVER_ERROR)
+        .send(authUtil.successFalse(responseMessage.INTERNAL_SERVER_ERROR))
+    })
+
+  if (!isCorrectPassword) {
+    return res.status(statusCode.BAD_REQUEST).send(authUtil.successFalse(responseMessage.MISS_MATCH_USER_INFO))
+  }
+  const { token } = jwt.sign(getUserInfoResult)
+  return res
+    .status(statusCode.OK)
+    .send(authUtil.successTrue(responseMessage.SIGN_IN_SUCCESS, { getUserInfoResult, token }))
 })
 
 // Find One by todoid
