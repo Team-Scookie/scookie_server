31a19d29fafcdb20b99e37b9d0fbd983f7bcf9b6
hyex <hyexkim@gmail.com>
2021-01-27 18:51:17 +0900

    test: add post /points api success case

--- tests/point.test.js
+++ tests/point.test.js
@@ -1,32 +1,46 @@
-// const mongoose = require("mongoose")
 const request = require("supertest")
 const app = require("../app")
-const dbManager = require("./index.js")
+const dbManager = require("./dbManager.js")
 
-describe("Temp test", () => {
-  // let connection = null
+afterAll(() => dbManager.close())
+beforeAll(() => dbManager.connect())
+// afterEach(() => dbManager.clear())
 
-  afterAll(() => dbManager.stop())
-  beforeAll(() => dbManager.connect())
-
-  // beforeAll(async () => {
-  //   connection = await mongoose
-  //     .connect(process.env.MONGO_URI, {
-  //       useNewUrlParser: true,
-  //       useUnifiedTopology: true,
-  //     })
-  //     .catch(e => console.error(e))
-  // })
-
-  // afterAll(async () => {
-  //   await connection.close()
-  // })
-
-  it("testing test", async () => {
-    const response = await request(app).get("/points")
-    console.log(response.status)
+describe("POST /points", () => {
+  test("success", async () => {
+    const body = {
+      userId: "test",
+      latitude: "37.586786",
+      longitude: "126.974800",
+      elapsedTime: 30000,
+    }
+    const response = await request(app).post("/points").send(body).expect(201)
     console.log(response.body)
-
-    expect(true).toBe(true)
   })
 })
+
+describe("GET /points", () => {
+  test("success", async () => {
+    const response = await request(app).get("/points").expect(200)
+    console.log(response.body)
+  })
+})
+
+/**
+ * {
+      success: true,
+      message: 'Point 작성 성공',
+      data: {
+        _id: '60112e9d3b9ab628f50f6816',
+        userId: 'test',
+        latitude: '37.5078166',
+        longitude: '126.9529052',
+        elapsedTime: 30000,
+        address: '서울특별시 동작구 매봉로2가길 11',
+        createdAt: '2021-01-27T09:13:01.259Z',
+        updatedAt: '2021-01-27T09:13:01.259Z',
+        __v: 0
+      }
+    }
+
+ */
