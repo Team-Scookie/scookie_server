dadabfb5ddf319516c5ff3940add307226edf148
100-yeeun <100yeeun@gmail.com>
2021-01-28 00:58:52 +0900

    Feat: create kakaoUtil to use kakao api

---
+++ tools/kakaoUtil.js
@@ -0,0 +1,23 @@
+const baseUrl = "https://dapi.kakao.com/v2/local/"
+const COUNT = 10
+
+const suggestPlace = async address => {
+  const places = await fetch(`${baseUrl}search/keyword.json?size=${COUNT}&sort=accuracy&query=${address}`, {
+    method: "GET",
+    headers: { Authorization: `KakaoAK ${process.env.KAKAO_KEY}` },
+  })
+  const placeList = await places.json()
+
+  if (!placeList.documents) {
+    return {
+        code: statusCode.BAD_REQUEST,
+        json: authUtil.successFalse(responseMessage.OUT_OF_VALUE),
+    }
+  }
+  return {
+    code: statusCode.OK,
+    json: authUtil.successTrue(responseMessage.X_READ_SUCCESS("Point"), placeList.documents),
+  }
+}
+
+module.exports = suggestPlace
