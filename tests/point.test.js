2a41830e4cc82221ae9ac7201b87fa87a7158224
hyex <hyexkim@gmail.com>
2021-01-27 17:05:29 +0900

    feat: add db manager for testing

--- tests/point.test.js
+++ tests/point.test.js
@@ -1,22 +1,26 @@
-const mongoose = require("mongoose")
+// const mongoose = require("mongoose")
 const request = require("supertest")
 const app = require("../app")
+const dbManager = require("./index.js")
 
 describe("Temp test", () => {
-  let connection = null
+  // let connection = null
 
-  beforeAll(async () => {
-    connection = await mongoose
-      .connect(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      })
-      .catch(e => console.error(e))
-  })
+  afterAll(() => dbManager.stop())
+  beforeAll(() => dbManager.connect())
 
-  afterAll(async () => {
-    await connection.close()
-  })
+  // beforeAll(async () => {
+  //   connection = await mongoose
+  //     .connect(process.env.MONGO_URI, {
+  //       useNewUrlParser: true,
+  //       useUnifiedTopology: true,
+  //     })
+  //     .catch(e => console.error(e))
+  // })
+
+  // afterAll(async () => {
+  //   await connection.close()
+  // })
 
   it("testing test", async () => {
     const response = await request(app).get("/points")
