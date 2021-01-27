8f5a9345b724be89b1240b3a75c1de0b804a8c56
hyex <hyexkim@gmail.com>
2021-01-27 15:42:23 +0900

    test: fixed import error

--- tests/point.test.js
+++ tests/point.test.js
@@ -1,13 +1,28 @@
+const mongoose = require("mongoose")
 const request = require("supertest")
 const app = require("../app")
 
 describe("Temp test", () => {
+  let connection = null
+
+  beforeAll(async () => {
+    connection = await mongoose
+      .connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .catch(e => console.error(e))
+  })
+
+  afterAll(async () => {
+    await connection.close()
+  })
+
   it("testing test", async () => {
     const response = await request(app).get("/points")
-    console.log(response)
+    console.log(response.status)
+    console.log(response.body)
 
     expect(true).toBe(true)
   })
 })
-
-jest.useFakeTimers()
