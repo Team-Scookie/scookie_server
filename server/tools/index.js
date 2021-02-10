08fbc838c79c76d856c42c4ed27ad0dd54c33cf2
100-yeeun <100yeeun@gmail.com>
2021-02-11 00:44:57 +0900

    Feat: add kakaoUtil

--- server/tools/index.js
+++ server/tools/index.js
@@ -2,7 +2,8 @@ const authUtil = require("./authUtil")
 const responseMessage = require("./responseMessage")
 const statusCode = require("./statusCode")
 const jwt = require("./jwt")
+const kakaoUtil = require("./kakaoUtil")
 
-const tools = { authUtil, responseMessage, statusCode, jwt }
+const tools = { authUtil, responseMessage, statusCode, jwt, kakaoUtil }
 
 module.exports = tools
